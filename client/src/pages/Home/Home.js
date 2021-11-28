10ad5a9d4f6440ec49aa3c0ece9821f94cc08cab
Varun <lanjharav@gmail.com>
2021-11-28 23:05:42 +0530

    Home page ui stuff done

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Home.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ background: "#fff" }}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                color: "#e44232",
+                fontSize: "27px",
+                marginLeft: "100px",
+                cursor: "pointer",
+              }}
+            >
+              WutToDo
+            </Typography>
+            <Button color="primary" style={{ marginRight: "20px" }}>
+              Login
+            </Button>
+            <Button color="primary" style={{ marginRight: "50px" }}>
+              Register
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="imgstuff">
+        <h1
+          style={{
+            position: "absolute",
+            left: "450px",
+            color: "#1f1f1f",
+            zIndex: 20,
+            fontSize: "72px",
+            top: "30px",
+            width: "600px",
+            opacity: 0.9,
+          }}
+        >
+          Organize it all with WutToDo
+        </h1>
+        <Button
+          variant="contained"
+          style={{
+            zIndex: 20,
+            position: "absolute",
+            left: "610px",
+            top: "340px",
+          }}
+        >
+          Get Started
+        </Button>
+        <img
+          src="../../images/home.jpg"
+          alt=""
+          style={{
+            width: "1000px",
+            height: "540px",
+            marginTop: "10px",
+            marginLeft: "150px",
+            opacity: 0.5,
+            zIndex: 10,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
