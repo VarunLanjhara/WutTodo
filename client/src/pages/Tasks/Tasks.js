784645e4afc0f5d01dbf7cd60e0230320e1341dc
Varun <lanjharav@gmail.com>
2021-11-29 00:17:20 +0530

    Navbar stuff done

--- client/src/pages/Tasks/Tasks.js
+++ client/src/pages/Tasks/Tasks.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Tasks = () => {
   return (
     <div>
-      <h1>Tasks</h1>
+      <Navbar />
     </div>
   );
 };
