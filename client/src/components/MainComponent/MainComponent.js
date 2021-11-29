e04c0a1c7fb4a7e00f8ece91f7f82a9d3afe3f4c
Varun <lanjharav@gmail.com>
2021-11-29 22:39:58 +0530

    Tasks topbar stuff done

--- client/src/components/MainComponent/MainComponent.js
+++ client/src/components/MainComponent/MainComponent.js
@@ -1,8 +1,56 @@
 import React from "react";
 import "./MainComponent.css";
+import CommentIcon from "@mui/icons-material/Comment";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import { IconButton } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const MainComponent = () => {
-  return <div className="MainComponent"></div>;
+  return (
+    <div className="MainComponent">
+      <div className="topbar">
+        <p
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "20px",
+            width: "300px",
+          }}
+        >
+          BlahBlahBlahBlahBlah
+        </p>
+        <div
+          className="commenthover"
+          style={{
+            display: "flex",
+            cursor: "pointer",
+            position: "relative",
+            left: "300px",
+          }}
+        >
+          <CommentIcon style={{ color: "white" }} />
+          <p style={{ fontWeight: "500", marginLeft: "5px" }}>Comments</p>
+        </div>
+        <div
+          className="sharehover"
+          style={{
+            display: "flex",
+            cursor: "pointer",
+            position: "relative",
+            left: "340px",
+          }}
+        >
+          <PersonAddAltIcon style={{ color: "white" }} />
+          <p style={{ fontWeight: "500", marginLeft: "5px" }}>Share</p>
+        </div>
+        <div style={{ position: "relative", left: "380px", bottom: "14px" }}>
+          <IconButton size="large">
+            <MoreHorizIcon style={{ color: "white" }} />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainComponent;
