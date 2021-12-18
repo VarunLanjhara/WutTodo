a77816b8e935ba2a39e1b8c80b31a6566f8ae185
Varun <lanjharav@gmail.com>
2021-12-18 21:01:24 +0530

    Fixed bugs

--- client/src/components/SearchResultProject/SearchResultProject.js
+++ client/src/components/SearchResultProject/SearchResultProject.js
@@ -1,6 +1,6 @@
 import { Checkbox } from "@mui/material";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { searchProjectTask } from "../../actions/projecttasks";
 import "./SearchResultProject.css";
 import { useParams } from "react-router-dom";
@@ -9,6 +9,7 @@ const SearchResultProject = () => {
   const dispatch = useDispatch();
   const params = useParams();
   const searchshit = params.searchshit;
+  const searchtasks = useSelector((searchtasks) => searchtasks.projectTasks);
   useEffect(() => {
     dispatch(searchProjectTask(searchshit));
   }, [dispatch, searchshit]);
@@ -27,70 +28,75 @@ const SearchResultProject = () => {
         </p>
       </div>
       <div style={{ marginLeft: "100px" }}>
-        {/* {searchtasks
-          ? searchtasks.map((task) => ( */}
-        <div
-          style={{
-            cursor: "pointer",
-            borderBottom: "1px solid gray",
-            width: "900px",
-            marginBottom: "12px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              cursor: "pointer",
-              width: "900px",
-            }}
-          >
-            <Checkbox
-              style={{
-                color: "white",
-                position: "relative",
-                bottom: "10px",
-              }}
-            />
+        {searchtasks
+          ? searchtasks.map((task) => (
+              <div
+                style={{
+                  cursor: "pointer",
+                  borderBottom: "1px solid gray",
+                  width: "900px",
+                  marginBottom: "12px",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    cursor: "pointer",
+                    width: "900px",
+                  }}
+                >
+                  <Checkbox
+                    style={{
+                      color: "white",
+                      position: "relative",
+                      bottom: "10px",
+                    }}
+                  />
+                  <p
+                    style={{
+                      color: "white",
+                      fontWeight: "bolder",
+                      width: "800px",
+                    }}
+                  >
+                    {task.name}
+                  </p>
+                </div>
+                <p
+                  style={{
+                    color: "gray",
+                    fontWeight: "bolder",
+                    fontSize: "15px",
+                    position: "relative",
+                    left: "43px",
+                    bottom: "14px",
+                    width: "800px",
+                  }}
+                >
+                  {task.description}
+                </p>
+              </div>
+            ))
+          : ""}
+        {searchtasks.length === 0 ? (
+          <div style={{ marginLeft: "250px", marginTop: "40px" }}>
+            <p style={{ color: "white", fontSize: "48px" }}>
+              Nothing To Show :(
+            </p>
             <p
               style={{
                 color: "white",
-                fontWeight: "bolder",
-                width: "800px",
+                fontSize: "48px",
+                marginLeft: "150px",
+                marginTop: "20px",
               }}
             >
-              Shit
+              Bruh
             </p>
           </div>
-          <p
-            style={{
-              color: "gray",
-              fontWeight: "bolder",
-              fontSize: "15px",
-              position: "relative",
-              left: "43px",
-              bottom: "14px",
-              width: "800px",
-            }}
-          >
-            Bruhhh
-          </p>
-        </div>
-        {/* )) : ""} */}
-        {/* {searchtasks.length === 0 ? ( */}
-        {/* <div style={{ marginLeft: "250px", marginTop: "40px" }}>
-          <p style={{ color: "white", fontSize: "48px" }}>Nothing To Show :(</p>
-          <p
-            style={{
-              color: "white",
-              fontSize: "48px",
-              marginLeft: "150px",
-              marginTop: "20px",
-            }}
-          >
-            Bruh
-          </p>
-        </div> */}
-        {/* ) : ( "" )} */}
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
