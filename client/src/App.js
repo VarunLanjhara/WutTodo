f2cd0af45211ec0472c2f7949c0cad1187217c44
Varun <lanjharav@gmail.com>
2021-12-20 22:43:47 +0530

    Cool scrollbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Preloader from "./components/Preloader/Preloader";
+import "./App.css";
 
 function App() {
   const Home = lazy(() => import("./pages/Home/Home"));
